acf88b3abeb34ffb7c64bcc2d36950409d589c25
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-08-28 14:06:44 -0400

    Adding opacity effect

--- _layout/_javascript/script.js
+++ _layout/_javascript/script.js
@@ -1,3 +1,21 @@
 'use strict';
 // ! *********** FOR LESSONS NAMES *********** //
 // ? *********** FOR REGULAR COMMENTS *********** //
+const items = document.querySelectorAll('.item');
+
+window.addEventListener('scroll', checkItems);
+
+checkItems();
+
+function checkItems() {
+  const trigger = (window.innerHeight / 5) * 4;
+  items.forEach(function (item) {
+    const itemTop = item.getBoundingClientRect().top;
+
+    if (itemTop < trigger) {
+      item.classList.add('show');
+    } else {
+      item.classList.remove('show');
+    }
+  });
+}
